b1321d10acb08b2bbf0d9d5a48b47f3c052ed480
harmanbatheja15 <harmanbatheja15@gmail.com>
2025-01-27 12:02:39 +0530

    portfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,247 @@
+import { Icons } from '@/components/icons';
+import { FileTextIcon } from 'lucide-react';
+
+export const DATA = {
+	name: 'Harman Batheja',
+	initials: 'HB',
+	url: 'https://harmanbatheja.tech',
+	location: 'Patiala, Punjab, India',
+	locationLink: '',
+	description: 'Full Stack Web Developer',
+	summary:
+		'Full Stack Developer with expertise in frontend and backend development.',
+	skills: [
+		'HTML',
+		'CSS',
+		'Tailwind CSS',
+		'JavaScript',
+		'Typescript',
+		'React',
+		'React Native',
+		'Node.js',
+		'Express',
+		'Django',
+		'Postgres',
+		'MongoDB',
+		'Prisma ORM',
+		'Recoil',
+		'Zustand',
+		'Git',
+		'AWS',
+	],
+	navbar: [
+		{
+			href: 'https://drive.google.com/file/d/1plT7rdzyj9zLalF4Ws6cqWKpYNnmye5A/view',
+			icon: FileTextIcon,
+			label: 'Resume',
+		},
+	],
+	contact: {
+		email: 'harmanbatheja15@gmail.com',
+		tel: '+918146800208',
+		social: {
+			GitHub: {
+				name: 'GitHub',
+				url: 'https://github.com/harmanbatheja15',
+				icon: Icons.github,
+
+				navbar: true,
+			},
+			LinkedIn: {
+				name: 'LinkedIn',
+				url: 'https://linkedin.com/in/harmanbatheja15',
+				icon: Icons.linkedin,
+
+				navbar: true,
+			},
+			X: {
+				name: 'X',
+				url: 'https://x.com/harmanbatheja15',
+				icon: Icons.x,
+
+				navbar: true,
+			},
+			email: {
+				name: 'Send Email',
+				url: '#',
+				icon: Icons.email,
+
+				navbar: false,
+			},
+		},
+	},
+
+	work: [
+		{
+			company: 'Kana Labs',
+			href: 'https://kanalabs.io',
+			badges: [],
+			location: 'Remote',
+			title: 'Frontend Intern',
+			logoUrl:
+				'https://app.kanalabs.io/_next/static/media/Small-logo.1b003dc5.svg',
+			start: 'July 2024',
+			end: 'Sep 2024',
+			description:
+				'Developed 10+ Landing Pages and frontend features using React and Next.js, ensuring smooth and responsive user experiences. Managed application state with Zustand to ensure efficient and predictable data flow across the app.',
+		},
+	],
+
+	education: [
+		{
+			school: 'Thapar Polytechnic College',
+			href: 'http://tpc.ac.in/',
+			degree: 'Diploma in Computer Science',
+			logoUrl: '#',
+			target: '_blank',
+			start: '2022',
+			end: '2025',
+		},
+		{
+			school: 'The Indo British School',
+			href: '#',
+			degree: 'Matriculation',
+			logoUrl: '#',
+			target: '',
+			start: '2012',
+			end: '2022',
+		},
+	],
+
+	projects: [
+		{
+			title: 'Course Selling Platform',
+			href: 'https://courseselling.xyz',
+			dates: '',
+			active: true,
+			description: `Created a platform allowing instructors to register and launch personalized website to sell their courses. Instructors can upload videos, set pricing, and manage content, while students can browse and purchase courses directly from each instructor's site.`,
+			technologies: [
+				'React',
+				'Typescript',
+				'Node.js',
+				'Express',
+				'PostgreSQL',
+				'Prisma',
+				'TailwindCSS',
+				'S3 Bucket',
+				'Razorpay',
+			],
+			links: [
+				{
+					type: 'Website',
+					href: 'https://courseselling.xyz',
+					icon: <Icons.globe className='size-3' />,
+				},
+				{
+					type: 'Source',
+					href: 'https://github.com/harmanbatheja15/course-selling-app',
+					icon: <Icons.github className='size-3' />,
+				},
+			],
+			image: '/course-selling.png',
+			video: '',
+		},
+		{
+			title: 'Alumni Association Website',
+			href: 'https://github.com/harmanbatheja15/AlumniAssociation',
+			dates: '',
+			active: true,
+			description:
+				'Built a role-based website for students, alumni, and faculty, allowing students to create profiles, explore job opportunities, and access news and events. The platform offers a personalized experience for each user type.',
+			technologies: [
+				'React',
+				'Node.js',
+				'Express',
+				'PostgreSQL',
+				'Prisma',
+				'TailwindCSS',
+			],
+			links: [
+				{
+					type: 'Source',
+					href: 'https://github.com/harmanbatheja15/AlumniAssociation',
+					icon: <Icons.github className='size-3' />,
+				},
+			],
+			image: '/alumni-association.png',
+			video: '',
+		},
+		{
+			title: `India's Got Latent Mobile App`,
+			href: 'https://github.com/harmanbatheja15/indias-got-latent',
+			dates: '',
+			active: true,
+			description:
+				'Developed a mobile app using React Native for Samay Raina, allowing users to watch his videos and purchase memberships.',
+			technologies: ['React Native', 'NativeWind'],
+			links: [
+				{
+					type: 'Source',
+					href: 'https://github.com/harmanbatheja15/indias-got-latent',
+					icon: <Icons.github className='size-3' />,
+				},
+			],
+			image: '/latent-app.png',
+			video: '',
+		},
+		{
+			title: 'Ice Staking Frontend',
+			href: 'https://ice-staking-frontend.vercel.app/',
+			dates: '',
+			active: true,
+			description:
+				'Designed, developed and sold animated UI components for developers.',
+			technologies: ['React', 'TailwindCSS'],
+			links: [
+				{
+					type: 'Website',
+					href: 'https://ice-staking-frontend.vercel.app/',
+					icon: <Icons.globe className='size-3' />,
+				},
+				{
+					type: 'Source',
+					href: 'https://github.com/harmanbatheja15/ice-staking-frontend',
+					icon: <Icons.github className='size-3' />,
+				},
+			],
+			image: '/ice-staking.png',
+			video: '',
+		},
+		{
+			title: 'Kanalabs: Landing Page',
+			href: 'https://landing-page-gamma-five-76.vercel.app/',
+			dates: '',
+			active: true,
+			description:
+				'Developed the main landing page for a Kana Labs, focusing on clean design, user engagement, and smooth navigation. Ensured responsive design for optimal viewing across devices and worked closely with the team to align the page with brand goals and users expectations.',
+			technologies: ['React', 'TailwindCSS'],
+			links: [
+				{
+					type: 'Website',
+					href: 'https://landing-page-gamma-five-76.vercel.app/',
+					icon: <Icons.globe className='size-3' />,
+				},
+			],
+			image: '/landing-page.png',
+			video: '',
+		},
+		{
+			title: 'Kanalabs: Redefining Perpetual Trading',
+			href: 'https://kanalabs-landing-page.vercel.app/',
+			dates: '',
+			active: true,
+			description:
+				'Created a dedicated landing page for Perpetual Trading for Kana Labs, highlighting key features and benefits.',
+			technologies: ['React', 'TailwindCSS'],
+			links: [
+				{
+					type: 'Website',
+					href: 'https://kanalabs-landing-page.vercel.app/',
+					icon: <Icons.globe className='size-3' />,
+				},
+			],
+			image: '/perpetual-trading.png',
+			video: '',
+		},
+	],
+} as const;
